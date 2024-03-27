b1e8805611ff7a735f9e99e8ddb481616182bf60
toan <taquoctoan1412@gmail.com>
2024-03-27 23:44:55 +0700

    hahah

---
+++ QL ChiTieu/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QL_ChiTieu.Models;
+
+namespace QL_ChiTieu.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
